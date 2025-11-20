9dcefa0c7f6f11f398892dc8223b555a88da94ad
Blakem07 <Blakem2020@gmail.com>
2025-11-20 16:52:17 +0000

    feat: Setup basic structure with inputForm and outputCV components

---
+++ src/components/InputForm.jsx
@@ -0,0 +1,5 @@
+import "../styles/InputForm.css";
+
+export default function InputForm() {
+  return <div className="input-form-container"></div>;
+}
---
+++ src/components/OutputCV.jsx
@@ -0,0 +1,5 @@
+import "../styles/OutputCV.css";
+
+export default function OutputCV() {
+  return <div className="output-cv-container"></div>;
+}
